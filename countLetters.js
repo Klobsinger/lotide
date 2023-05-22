c5080fd87a2b411747ccc00573ece143c7fb38f3
Kevin Lobsinger <kevlobsinger@gmail.com>
2023-05-21 20:04:58 -0400

    added the continue method into my for of loop to skip spaces added the assertEqual with Json.stringify for testing and cleaned up the code

--- countLetters.js
+++ countLetters.js
@@ -6,23 +6,35 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-const countLetters = function(sentence){
-let lettersCounted = {}           //creating a blank object 
-  for( const letters of sentence){ // using the for of loop to loop through the string given to the function
+const countLetters = function(sentence) {
+  let lettersCounted = {};          //creating a blank object
+  for (const letters of sentence) { // using the for of loop to loop through the string given to the function
+    if (letters === ' ') {         // I found the continue statement online and used a if statement to find spaces
+      continue;                    //and to skip to the next loop right away if it finds a space so they arent counted
+    }
     if (lettersCounted[letters]) { //using the same if else statement as the previous assingment to check for truthy
       lettersCounted[letters] += 1; //or falsey if false the letter hasnt appeared yet and is undefined so we make its value 1
-    } else {                        // if true it already exists isnt undefined so its value is increased by 1
+    } else {                        // if true it already exists, isnt undefined so its value is increased by 1
       lettersCounted[letters] = 1;
+    }
   }
-  }
 
 
-return lettersCounted
-}
-
-
-console.log(countLetters("lighthouse in the house"))
-
-//each letter gets returned as a key and its value is the amount of times its shown
-//increase the count each time that 
-//assign the values of the object based on the letters passed through?
+  return lettersCounted;
+};
+const result = countLetters("lighthouse in the house");
+const expected = {
+  l: 1,
+  i: 2,
+  g: 1,
+  h: 4,
+  t: 2,
+  o: 2,
+  u: 2,
+  s: 2,
+  e: 3,
+  n: 1,
+};
+assertEqual(JSON.stringify(result), JSON.stringify(expected));    // I was not sure how to do the previous test from countOnly.js
+                                                                  //online i found the json.stringify method which converts objects to strings
+                                                                  //and then compares the values? i believe this works for testing
