78b19873003638e47c7d30720fe7a41e0e36dd77
Kevin Lobsinger <kevlobsinger@gmail.com>
2023-06-06 17:35:41 -0400

    refactored previous lotide code to seperate the tests from the functions and used exporting rather then copy and pasting.

--- assertEqual.js
+++ assertEqual.js
@@ -5,7 +5,7 @@ const assertEqual = function(actual, expected) {
     console.log(`🛑🛑🛑  Assertion Failed ${actual}  !==  ${expected}`);
   }
 };
-assertEqual("Lighthouse Labs", "Lighthouse Labs");
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual(1, 2);
+
+
+
+module.exports = assertEqual
